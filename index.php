efcb1357f13e33ec48c6c6d5a3154328208d2a33
SneakyAnimeow <sneakyanimeow@anims.club>
2022-09-06 09:58:26 +0200

    first

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $site = $_GET['site'];
+
+    if($site == ""){
+        $site = "main";
+    }
+
+    $content = file_get_contents("sites/$site.php");
+    $constants = json_decode(file_get_contents("jsons/$site.json"), true);
+
+    foreach($constants as $key => $value){
+        $content = str_replace("@$key", (is_array($value) ? implode(", ", $value) : $value), $content);
+    }
+
+    echo $content;
+?>
+</html>
